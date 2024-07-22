0596d69b1f74da5b0688e45bcfd44721e8f2ff4e
uncommonlibrary <carmen916@gmail.com>
2024-07-22 15:26:33 +0800

    fixed inventory page

--- main2.js
+++ main2.js
@@ -1,3 +1,8 @@
+/*-------------------------------- Constants --------------------------------*/
+const playB = document.getElementById("playB");
+const startPullB = document.getElementById("startPullB");
+const startTradeB = document.getElementById("startTradeB");
+
 /*----------------------------- Game Pages --------------------------------*/
 const titleP = document.getElementById("title");
 const startP = document.getElementById("start");
@@ -37,24 +42,31 @@ const tradePage = [
   },
 ];
 
+const inventory = [];
+
 /*-------------------------------Random Functions------------------------------*/
+//to pull only regular sonny angels (FOR FIRST PULL ONLY)
+const regularSAs = sonnyAngels.filter((angel) => angel.type === "regular"); //filters for only regular SAs
+function getRandomRegSA() {
+  const regSAIndex = Math.floor(Math.random() * regularSAs.length); //chooses random index in regulars
+  selectedRegSA = regularSAs[regSAIndex];
+  return selectedRegSA;
+}
+
 function getRandomSA() {
   const randomSAIndex = Math.floor(Math.random() * sonnyAngels.length); //chooses random index in all
   selectedSATrade = sonnyAngels[randomSAIndex];
   return selectedSATrade; //outputs the selected sonny Object
 }
 
-/*---------------------------------Functions------------------------------*/
-// function firstTrade() {
-//     tradePage[0].h1Text.push("TRADE EVENT");
-//     tradePage[0].text.push(
-//       "Someone responded to your trade request! Here's what they have up for trade. Choose wisely!"
-//     );
-// }
-// firstTrade();
+//function to log blindbox sonny to inventory
+function addRandomSonny(randomSonny) {
+  inventory.push(randomSonny); //adding randomly selected sonny into player inventory
+  console.log("Current Inventory:", inventory);
+}
 
+/*---------------------------------TRADE Functions------------------------------*/
 function firstTrade() {
-  const firstTradeP = document.getElementById("firstTrade");
   firstTradeP.innerHTML = ""; //clear previous content if any
 
   const h1Text = document.createElement("h1");
@@ -64,7 +76,7 @@ function firstTrade() {
   const h3Text = document.createElement("h3");
   h3Text.textContent = tradePage[0].h3Text;
   firstTradeP.appendChild(h3Text);
-  
+
   for (let i = 0; i < 3; i++) {
     const tradeDiv = document.createElement("div"); //create 3 divs
     tradeDiv.setAttribute("class", "tradeOption");
@@ -72,15 +84,19 @@ function firstTrade() {
   }
 
   displayTradeOptions();
-  titleP.style.display = "none";
+  startResultP.style.display = "none";
   firstTradeP.style.display = "block";
 }
 
 function displayTradeOptions() {
   const tradeDivs = firstTradeP.querySelectorAll(".tradeOption"); //selects all trade option class in first trade page
-  for (let i = 0; i < tradeDivs.length; i++) { //in each div do this (should run 3 times)
+  const tradeOptions = []; //to store the trade options so they dont disappear after for loop
+  
+  for (let i = 0; i < tradeDivs.length; i++) {
+    //in each div do this (should run 3 times)
     const tradeOption = getRandomSA(); //generate random sonny
-    
+    tradeOptions.push(tradeOption); //adds trade option to array
+
     const tradeOptionImg = document.createElement("img"); //create image element in each div
     tradeOptionImg.src = tradeOption.image; //gets image URL from trade option
     tradeOptionImg.setAttribute("id", `tradeOption${i}`); //giving specific id to each trade option
@@ -89,68 +105,78 @@ function displayTradeOptions() {
     const tradeSelectButton = document.createElement("button");
     tradeSelectButton.setAttribute("id", `tradeSelectButton${i}`);
     tradeSelectButton.innerHTML = "Select";
+    tradeSelectButton.addEventListener("click", () => {
+        inventory.push(tradeOptions[i]); //adds event listener to each button and registers the click to inventory
+        console.log("Chosen Sonny:", tradeOptions[i]);
+        console.log("Current Inventory:", inventory);
+        displayFirstTradeInventory();
+    })
     tradeDivs[i].appendChild(tradeSelectButton); //appends select button to specific trade div
   }
 }
-//   const tradeOption1 = getRandomSA(); //generate options
-//   const tradeOption2 = getRandomSA();
-//   const tradeOption3 = getRandomSA();
 
-  //   const tradeOption1Img = document.createElement("img"); //create img element
-  //   const tradeOption2Img = document.createElement("img");
-  //   const tradeOption3Img = document.createElement("img");
-  //   const tradeOptionEl1 = document.getElementById("tradeOption1");
-  //   const tradeOptionEl2 = document.getElementById("tradeOption2");
-  //   const tradeOptionEl3 = document.getElementById("tradeOption3");
-  //   //select buttons
-  //   const tradeSelect1 = document.createElement("button");
-  //   tradeSelect1.setAttribute("id", "tradeSelect1");
-  //   tradeSelect1.innerHTML = "Select";
-  //     const tradeSelect2 = document.createElement("button");
-  //     tradeSelect2.setAttribute("id", "tradeSelect2");
-  //     tradeSelect2.innerHTML = "Select";
-  //       const tradeSelect3 = document.createElement("button");
-  //       tradeSelect3.setAttribute("id", "tradeSelect3");
-  //       tradeSelect3.innerHTML = "Select";
+/*----------------------------- Start Pull Page -----------------------------*/
+playB.addEventListener("click", displayStartPullP);
 
-  //   tradeOption1Img.src = tradeOption1.image; //added the image
-  //   tradeOption2Img.src = tradeOption2.image;
-  //   tradeOption3Img.src = tradeOption3.image;
+function displayStartPullP() {
+    titleP.style.display = "none";
+    startP.style.display = "block";
+}
+/*--------------------------------------------------------------------------*/
 
-  //   tradeOptionEl1.appendChild(tradeOption1Img); //append image to parent firstTradeP
-  //   tradeOptionEl2.appendChild(tradeOption2Img);
-  //   tradeOptionEl3.appendChild(tradeOption3Img);
+/*----------------------------- Start Pull Result Page -----------------------------*/
+startPullB.addEventListener("click", displayStartResultP);
 
-  //   firstTradeP.appendChild(tradeOptionEl1);
-  //   firstTradeP.appendChild(tradeOptionEl2);
-  //   firstTradeP.appendChild(tradeOptionEl3);
-  //   tradeOptionEl1.appendChild(tradeSelect1);
-  //     tradeOptionEl2.appendChild(tradeSelect2);
-  //     tradeOptionEl3.appendChild(tradeSelect3);
+function displayStartResultP() {
+    startP.style.display = "none";
+    startResultP.style.display = "block"
+    displayRandomRegSonny();
+}
 
-// console.log(tradeDivs);
-// for (let i = 0; i < tradeDivs.length; i++) {
-//   const image = document.createElement("img");
-//   image.src = `tradeOption${i + 1}.image`;
-//   const button = document.createElement("button");
-//   button.setAttribute("id", `tradeSelect${i + 1}`);
-//   button.innerHTML = "Select";
-//   tradeDivs[i].appendChild(image);
-//   tradeDivs[i].appendChild(button);
-// }
-// }
+function displayRandomRegSonny() {
+    const startSonny = getRandomRegSA(); //randomly selects a regular sonny
+    const startSonnyImg = document.getElementById("startSonnyImg");
+    startSonnyImg.src = startSonny.image; //displays the URL of random start Sonny
+    addRandomSonny(startSonny); //adds the start sonny into inventory array
+}
+/*--------------------------------------------------------------------------*/
 
-// const tradeDivs = firstTradeP.querySelectorAll("div")
-// for (let i = 0; i < tradeDivs.length; i++) {
-//     const image = document.createElement("img");
-//     // const tradeOptionImg = tradeDivs[i].createElement("img");
-//     image.src = `tradeOption${[i+1]}.image`;
-//     const button = document.createElement("button");
-//     button.setAttribute("id", `tradeSelect${[i+1]}`);
-//     button.innerHTML = "Select";
-//     tradeDivs[i].appendChild(image);
-//     tradeDivs[i].appendChild(button);
-// }
+/*----------------------INVENTORY RELATED-----------------------*/
+startTradeB.addEventListener("click", firstTrade)
 
-const playB = document.getElementById("playB");
-playB.addEventListener("click", firstTrade);
+function displayFirstTradeInventory() {
+    firstTradeP.style.display = "none";
+    firstTradeInventoryP.style.display = "block";
+
+    const h1Text = document.createElement("h1");
+    h1Text.textContent = "TRADE EVENT";
+    firstTradeInventoryP.appendChild(h1Text);
+    
+    const h3Text = document.createElement("h3");
+    h3Text.textContent = "Which Sonny do you want to give up?";
+    firstTradeInventoryP.appendChild(h3Text);
+
+    for (let i = 0; i < inventory.length; i++) {
+        const inventoryImg = document.createElement("img"); //create img element for each inventory item
+        inventoryImg.src = inventory[i].image; //gets URL link from inventory
+        firstTradeInventoryP.appendChild(inventoryImg);
+
+        const inventorySelectButton = document.createElement("button"); //create button for every inventory option
+        inventorySelectButton.setAttribute("id", `inventorySelectButton${i}`);
+        inventorySelectButton.innerHTML = "Give this up!";
+        inventorySelectButton.addEventListener("click", () => {
+            inventory.splice(i, 1); //start at index, deletes only 1
+            console.log("Current Inventory:", inventory);
+            displayFirstTradeSuccess();
+        })
+        firstTradeInventoryP.appendChild(inventorySelectButton);
+    }
+}
+/*--------------------------------------------------------------------------*/
+
+/*--------------------display first trade success---------------------------*/
+function displayFirstTradeSuccess() {
+    firstTradeInventoryP.style.display = "none";
+    firstTradeSuccessP.style.display = "block";
+}
+/*--------------------------------------------------------------------------*/
